0f65c781356b0b796f9a08a50b13fd9a2f33de6a
Anandhu Vijayan <38665597+Anandhuvijayan@users.noreply.github.com>
2021-02-18 13:23:33 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -40,16 +40,17 @@ const quizData = [
     correct: "b",
   },
 ];
-
+const quiz = document.getElementById("quiz");
+const answers = document.querySelectorAll(".answers");
 const qust = document.getElementById("question");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
+const submitBut = document.getElementById("submit");
 
 let currentQuiz = 0;
 
-loadQuiz();
 function loadQuiz() {
   const currentQuizData = quizData[currentQuiz];
   qust.innerText = currentQuizData.question;
@@ -57,6 +58,27 @@ function loadQuiz() {
   b_text.innerText = currentQuizData.b;
   c_text.innerText = currentQuizData.c;
   d_text.innerText = currentQuizData.d;
-
-  currentQuiz++;
 }
+loadQuiz();
+
+// const answer = currentQuizData.correct;
+// console.log(answer);
+
+function getSelected() {
+  for (let i = 0; i < answers.length; i++) {
+    if (answer.checked) {
+      answer = answers.id;
+    }
+  }
+  return answer;
+}
+
+submitBut.addEventListener("click", () => {
+  currentQuiz++;
+
+  if (currentQuiz < quizData.length) {
+    loadQuiz();
+  } else {
+    quiz.innerHTML = "ehdquf";
+  }
+});
