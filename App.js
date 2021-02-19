b8affc3533fabcee13ad81f060518c9b6b39fa6b
Anandhu Vijayan <38665597+Anandhuvijayan@users.noreply.github.com>
2021-02-19 12:29:34 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -41,7 +41,7 @@ const quizData = [
   },
 ];
 const quiz = document.getElementById("quiz");
-const answers = document.querySelectorAll(".answers");
+const answers = document.querySelectorAll(".answer");
 const qust = document.getElementById("question");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
@@ -50,8 +50,12 @@ const d_text = document.getElementById("d_text");
 const submitBut = document.getElementById("submit");
 
 let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
 
 function loadQuiz() {
+  deselectAnswers();
   const currentQuizData = quizData[currentQuiz];
   qust.innerText = currentQuizData.question;
   a_text.innerText = currentQuizData.a;
@@ -59,26 +63,41 @@ function loadQuiz() {
   c_text.innerText = currentQuizData.c;
   d_text.innerText = currentQuizData.d;
 }
-loadQuiz();
-
-// const answer = currentQuizData.correct;
-// console.log(answer);
 
 function getSelected() {
-  for (let i = 0; i < answers.length; i++) {
-    if (answer.checked) {
-      answer = answers.id;
+  let answer = undefined;
+
+  for (var i = 0, len = answers.length; i < len; i++) {
+    if (answers[i].checked) {
+      answer = answers[i].id;
+      break;
     }
   }
   return answer;
 }
 
+function deselectAnswers() {
+  answers.forEach((answerEl) => {
+    answerEl.checked = false;
+  });
+}
 submitBut.addEventListener("click", () => {
-  currentQuiz++;
+  const answer = getSelected();
 
-  if (currentQuiz < quizData.length) {
-    loadQuiz();
-  } else {
-    quiz.innerHTML = "ehdquf";
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++;
+    }
+
+    currentQuiz++;
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHTML = `
+        <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+        
+        <button onclick="location.reload()">Reload</button>
+    `;
+    }
   }
 });
